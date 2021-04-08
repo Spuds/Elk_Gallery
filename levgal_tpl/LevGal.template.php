2f9b97e3c9c610ddedb119dc6b161f605f572367
Spuds <spuds@spudsdesign.com>
2021-04-08 08:47:15 -0500

    ! missed another PNG->SVG

--- levgal_tpl/LevGal.template.php
+++ levgal_tpl/LevGal.template.php
@@ -12,9 +12,6 @@ function template_main()
 {
 	template_main_page_sidebar();
 	template_main_page_display();
-
-	echo '
-		<br class="clear" />';
 }
 
 function template_main_page_sidebar()
@@ -459,7 +456,7 @@ function template_album_list_main()
 				echo '
 				<div class="album_featured well">
 					<div class="floatleft album_thumb">
-						<img src="', $settings['default_theme_url'], '/levgal_res/albums/folder-image.png" alt="" />
+						<img src="', $settings['default_theme_url'], '/levgal_res/albums/folder-image.svg" alt="" />
 					</div>
 					<div class="album_desc lefttext">
 						<a href="', $group['url'], '">', $group['title'], '</a><br />
