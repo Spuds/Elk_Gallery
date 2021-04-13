8fe29e3c7452a9309d37e764650b6e3f25ead244
Spuds <spuds@spudsdesign.com>
2021-04-13 11:43:10 -0500

    ! misc cleanup

--- levgal_src/Helper/Richtext.php
+++ levgal_src/Helper/Richtext.php
@@ -76,18 +76,17 @@ class LevGal_Helper_Richtext
 	{
 		global $context, $txt;
 
-		// We would use SMF's template_control_richedit_buttons if it weren't useless to us and not customisable.
 		$editor_context = &$context['controls']['richedit'][$this->form_var];
 
 		echo '
 				<input type="hidden" name="', $context['session_var'], '" value="', $context['session_id'], '" />
 				<p id="post_confirm_buttons" class="righttext">
-					<input type="submit" value="', $editor_context['labels']['post_button'] ?? $txt['post'], '" tabindex="', $context['tabindex']++, '"', isset($editor_context['js']['post_button']) ? ' onclick="' . $editor_context['js']['post_button'] . '"' : '', ' class="button_submit" />';
+					<input type="submit" value="', $editor_context['labels']['post_button'] ?? $txt['post'], '" tabindex="', $context['tabindex']++, '"', isset($editor_context['js']['post_button']) ? ' onclick="' . $editor_context['js']['post_button'] . '"' : '', ' />';
 
 		if ($editor_context['preview_type'])
 		{
 			echo '
-					<input type="submit" name="preview" value="', $editor_context['labels']['preview_button'] ?? $txt['preview'], '" tabindex="', $context['tabindex']++, '"', isset($editor_context['js']['preview_button']) ? ' onclick="' . $editor_context['js']['preview_button'] . '"' : '', ' class="button_submit" />';
+					<input type="submit" name="preview" value="', $editor_context['labels']['preview_button'] ?? $txt['preview'], '" tabindex="', $context['tabindex']++, '"', isset($editor_context['js']['preview_button']) ? ' onclick="' . $editor_context['js']['preview_button'] . '"' : '', ' />';
 		}
 
 		echo '
